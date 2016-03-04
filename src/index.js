b0083ed901321469ee2841ae00bbca17f4561d67
falconzs <falconfiftyfive@yahoo.co.uk>
2016-03-04 20:13:12 +0000

    Initial commit of cleaner plugin

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+var DefineClass = require('define-class'),
+    PluginAbstract = require('./abstract'),
+    CleanerPlugin = DefineClass(PluginAbstract, {
+
+        init: function() {
+            this.commands = {
+                cleanup: {
+                    access: ['host', 'mod'],
+                    handler: this.cleanup,
+                    description: '',
+                    params: /(\d+)/
+                }
+            };
+            this._super();
+        },
+
+        cleanup: function(fromUser, quantity) {
+            var count,
+                backgroundColor = this.library.colors.blue;
+            if (!quantity || !Number.parseInt(quantity, 10)) {
+                quantity = 30;
+            }
+            for (count = 1; count <= quantity; count++) {
+                this.api.sendNotice("Cleaning up chat...", '', backgroundColor);
+            }
+            this.api.sendNotice("Chat cleaned up.", '', backgroundColor);
+        }
+    });
+
+module.exports = CleanerPlugin;
